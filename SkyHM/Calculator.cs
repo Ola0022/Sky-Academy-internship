7a4f9918b3239d916f4a9720606b429b5127d86e
Ola0022 <153562599+Ola0022@users.noreply.github.com>
2024-12-27 02:05:02 +0300

    ObjectHM

---
+++ SkyHM/Calculator.cs
@@ -0,0 +1,22 @@
+﻿class Calculator{
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public Calculator(int X, int Y){
+        x = X; y = Y;
+    }
+    public int Add(){
+        return x + y;
+    }
+    public int Subtract(){
+        return x - y;
+    }
+    public int Multiply(){
+        return x * y;
+    }
+    public string Divide(){
+        if (y == 0) return "Cannot divide by zero";
+        return (x / y).ToString();
+    }
+}
+
---
+++ SkyHM/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program{
+    static void Main(String[] args){
+        Console.Write("Enter value of x:");
+        int x=Convert.ToInt16(Console.ReadLine());
+
+        Console.Write("Enter value of y:");
+        int y = Convert.ToInt16(Console.ReadLine());
+
+        Calculator calc1= new Calculator(x,y);
+        
+        Console.WriteLine("Sum (x + y)= "+ calc1.Add());
+
+        Console.WriteLine("Difference (x - y)= " + calc1.Subtract());
+        
+        Console.WriteLine("Product (x * y)= " + calc1.Multiply());
+        
+        Console.WriteLine("Quotient (x / y)= " + calc1.Divide());
+        
+    }
+}
